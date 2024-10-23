b1b2b5bc175b25a189f01e67f803057023791eb6
Shane Freeder <theboyetronic@gmail.com>
2024-10-23 09:51:43 +0100

    add some metadata for pots

--- src/app/post/[postId]/page.jsx
+++ src/app/post/[postId]/page.jsx
@@ -1,13 +1,13 @@
-import { CommentForm } from "@/components/CommentForm";
-import { CommentList } from "@/components/CommentList";
-import { Vote } from "@/components/Vote";
-import { db } from "@/db";
+import {CommentForm} from "@/components/CommentForm";
+import {CommentList} from "@/components/CommentList";
+import {Vote} from "@/components/Vote";
+import {db} from "@/db";
 
-export default async function SinglePostPage({ params }) {
-  const postId = params.postId;
+export default async function SinglePostPage({params}) {
+    const postId = params.postId;
 
-  const { rows: posts } = await db.query(
-    `SELECT posts.id, posts.title, posts.body, posts.created_at, users.name, 
+    const {rows: posts} = await db.query(
+        `SELECT posts.id, posts.title, posts.body, posts.created_at, users.name, 
     COALESCE(SUM(votes.vote), 0) AS vote_total
     FROM posts
     JOIN users ON posts.user_id = users.id
@@ -15,28 +15,51 @@ export default async function SinglePostPage({ params }) {
     WHERE posts.id = $1
     GROUP BY posts.id, users.name
     LIMIT 1;`,
-    [postId]
-  );
-  const post = posts[0];
+        [postId]
+    );
+    const post = posts[0];
 
-  const { rows: votes } = await db.query(
-    `SELECT *, users.name from votes
+    const {rows: votes} = await db.query(
+        `SELECT *, users.name from votes
      JOIN users on votes.user_id = users.id`
-  );
+    );
 
-  return (
-    <div className="max-w-screen-lg mx-auto pt-4 pr-4">
-      <div className="flex space-x-6">
-        <Vote postId={post.id} votes={post.vote_total} />
-        <div className="">
-          <h1 className="text-2xl">{post.title}</h1>
-          <p className="text-zinc-400 mb-4">Posted by {post.name}</p>
+    return (
+        <div className="max-w-screen-lg mx-auto pt-4 pr-4">
+            <div className="flex space-x-6">
+                <Vote postId={post.id} votes={post.vote_total}/>
+                <div className="">
+                    <h1 className="text-2xl">{post.title}</h1>
+                    <p className="text-zinc-400 mb-4">Posted by {post.name}</p>
+                </div>
+            </div>
+            <main className="whitespace-pre-wrap m-4">{post.body}</main>
+
+            <CommentForm postId={post.id}/>
+            <CommentList postId={post.id}/>
         </div>
-      </div>
-      <main className="whitespace-pre-wrap m-4">{post.body}</main>
+    );
+}
 
-      <CommentForm postId={post.id} />
-      <CommentList postId={post.id} />
-    </div>
-  );
+const defaultMetadata = {
+    title: "Didit",
+    description: "A social app like Reddit or Hacker News",
+}
+
+export async function generateMetadata({params}) {
+    const postId = params.postId;
+    const res = await db.query(
+        `SELECT posts.title 
+            FROM posts
+        WHERE post.id = $1`,
+        [postId])
+    if (res.rowCount === 0) {
+        return defaultMetadata;
+    }
+
+    return {
+        ...defaultMetadata,
+        title: res.rows[0].title + " | Didit",
+    };
+
 }
